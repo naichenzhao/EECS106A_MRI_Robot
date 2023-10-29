4bac24448034a84d0b42828cf6578ffcda91419f
naichenzhao <naichenzhao13@gmail.com>
2023-10-29 02:48:30 -0700

    initial ros workspace

---
+++ ros_workspaces/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/naichenzhao/Desktop/EECS106A_Project/ros_workspaces/src
+# Build directory: /home/naichenzhao/Desktop/EECS106A_Project/ros_workspaces/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("comm_test")
