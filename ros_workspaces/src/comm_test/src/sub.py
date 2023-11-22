0fadeef4ce72744554e04593acc68997a35defdb
naichenzhao <naichenzhao13@gmail.com>
2023-11-22 13:50:02 -0800

    updated pub

--- ros_workspaces/src/comm_test/src/sub.py
+++ ros_workspaces/src/comm_test/src/sub.py
@@ -25,6 +25,20 @@ def listener():
 
 if __name__ == '__main__':
     rospy.init_node('listener', anonymous=True)
-    STM = serial.Serial(port='/dev/ttyACM1', baudrate=115200, timeout=.1)
+
+    STM = None
+
+    # Test different ports until one sticks. If not, rais an error
+    for i in range(10):
+        try:
+            STM = serial.Serial(port='/dev/ttyACM' + str(i), baudrate=115200, timeout=.1)
+            print("connecting to STM32 on port: /dev/ttyACM"+ str(i))
+            break
+        except:
+            print("error connecting to port /dev/ttyACM" + str(i))
+            print("     retrying on a different port...")        
+    
+    if STM == None:
+        raise Exception("Unable to detect STM32 controller")
 
     listener()
