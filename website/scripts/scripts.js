dc68aaacb945662f5e485dd086e39a371fc5771f
Charles Paxson <30535060+chpaxson@users.noreply.github.com>
2023-12-13 09:49:19 -0800

    Update website

--- website/scripts/scripts.js
+++ website/scripts/scripts.js
@@ -15,3 +15,34 @@ window.addEventListener('scroll', function() {
         }
     });
 });
+
+const csv_path = '/ros_workspaces/src/head_gui/src/points.csv';
+// const csv_path = 'https://raw.githubusercontent.com/naichenzhao/EECS106A_Project/main/ros_workspaces/src/head_gui/src/points.csv?token=GHSAT0AAAAAACBB4JT7VLYBQMCGI4OIDZUMZLZ3IRQ';
+
+d3.csv(csv_path, function(err, rows){
+function unpack(rows, key) {
+	return rows.map(function(row)
+	{ return row[key]; });}
+
+var trace1 = {
+	x:unpack(rows, 'Point_X'), y: unpack(rows, 'Point_Y'), z: unpack(rows, 'Point_Z'),
+	mode: 'markers',
+	marker: {
+		size: 5,
+		line: {
+		color: 'rgba(217, 217, 217, 0.14)',
+		width: 0.25},
+		opacity: 0.8},
+	type: 'scatter3d'
+};
+
+var data = [trace1];
+var layout = {margin: {
+	l: 0,
+	r: 0,
+	b: 0,
+	t: 0
+  }};
+Plotly.newPlot('head_gui_plot', data, layout);
+});
+
